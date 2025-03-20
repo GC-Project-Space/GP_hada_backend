f9ac3257e237858e7b237b0f0052a0059fd5bfc9
shimfff <shimseawon1510@gmail.com>
2025-03-20 23:04:26 +0900

    update project

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import {Logger, Module, OnApplicationBootstrap} from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { OpenAIModule } from './domain/openai/openai.module';
+import { ChatModule } from './domain/chat/chat.module';
+import { KakaoModule } from './domain/kakao/kakao.module';
+import { configValidationSchema } from './config/validation/config-validation';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [OpenAIModule, ChatModule, KakaoModule, ConfigModule.forRoot({
+    isGlobal: true,
+    cache: true,
+    envFilePath: [ '.env.' + process.env.NODE_ENV ], // 실행 환경에 따라 .env 파일을 선택
+    validationSchema: configValidationSchema,
+  }),
+    // MongoDB 연결
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], // ConfigModule 로드
+      inject: [ConfigService], // ConfigService 주입
+      useFactory: (configService: ConfigService) => {
+        const dbHost = configService.get<string>('DB_HOST');
+
+        const uri = `${dbHost}`;
+
+        return { uri };
+
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
